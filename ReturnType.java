24258101828eb850a46deeaea763d2fba7c80e4d
saiguruG <17saiguru@gmail.com>
2024-11-15 03:52:31 -0800

    Add files via upload

---
+++ ReturnType.java
@@ -0,0 +1,34 @@
+package javaproject;
+
+public class ReturnType {
+	
+	
+	public static  int plus (int sub1,int sub2) {
+		
+		
+		return(sub1+sub2);
+		
+	}
+		
+		
+		public static void main(String[]args) {
+			
+			
+		
+			
+			//int result =sub1+sub2;
+			
+			System.out.println("TOTAL MARK OF SUB  1 AND 2:" +plus(12,16));
+			
+			
+		}
+		
+		
+		
+	
+		
+	
+	
+	
+
+}
