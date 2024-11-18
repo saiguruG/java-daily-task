355d9976d92a523d1e591494c9e93eeebfea0fcd
saiguruG <17saiguru@gmail.com>
2024-11-18 09:52:31 -0800

    Update leapyearchecker.java

--- leapyearchecker.java
+++ leapyearchecker.java
@@ -10,7 +10,7 @@ public class leapyearchecker {
 	int year = object.nextInt();
 	//if(yeae% 4==a&& year%100!=a)\\(year%400==0))
 	
-		if ((year % 4 == 0 && year%100!=0)||(year%400==0)){
+		if (year % 4 == 0 && year%100!=0){
 			
 			System.out.println(year+ "   It is leap year: "); 
 	
