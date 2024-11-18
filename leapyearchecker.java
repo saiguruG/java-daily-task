1d4c4e6c156b5aaf9eb461af9f40ea89b6258a91
saiguruG <17saiguru@gmail.com>
2024-11-18 09:47:28 -0800

    Add files via upload

---
+++ leapyearchecker.java
@@ -0,0 +1,24 @@
+package javaproject;
+import java.util.Scanner;
+
+public class leapyearchecker {
+	public static void main(String[]args) {
+	Scanner object =new Scanner(System.in);
+	
+	System.out.println("enter the year: ");
+	
+	int year = object.nextInt();
+	//if(yeae% 4==a&& year%100!=a)\\(year%400==0))
+	
+		if ((year % 4 == 0 && year%100!=0)||(year%400==0)){
+			
+			System.out.println(year+ "   It is leap year: "); 
+	
+		}else { 
+	
+		System.out.println( year + "    It not a leap year:" );
+		
+	}
+	
+  
+}}
