380cd1362953387f70eab66ef3f06e4cee787beb
saiguruG <17saiguru@gmail.com>
2024-11-18 21:37:57 -0800

    Add files via upload

---
+++ Loopmultipeof10.java
@@ -0,0 +1,17 @@
+package javaproject;
+
+public class Loopmultipeof10 {
+	public static void main(String[]args) {
+		//Initialize the variable
+		int num=10;
+		//use of while loop to print number form 10 to 100;
+		while(num<=100) {
+			
+			System.out.println("enter the multiples of 10: "  +num);
+			num +=10;
+		}
+		
+		
+	}
+
+}
