1186704f9e0911b623a0dfb0e856f8a3359ee91f
saiguruG <17saiguru@gmail.com>
2024-11-19 02:55:28 -0800

    Update EvenorAdd.java

--- EvenorAdd.java
+++ EvenorAdd.java
@@ -2,7 +2,7 @@ package javaproject;
 
 public class EvenorAdd {
 	public static void printEvenorOdd(int[]numbers) {
-		int[] number = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24};
+		int[] number = {1,2,3,4,5,6,7,8,9,10};
 		//loop through each number in the array
 		for (int num: numbers) {System.out.println(" its even: ");
 			if(num% 2==0) {
@@ -19,7 +19,7 @@ public class EvenorAdd {
 		
 	}
 	public static void main (String[]args ) {
-		int[] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,27,18,19,20,21,22,23,24};
+		int[] numbers = {1,2,3,4,5,6,7,8,9,10};
 		printEvenorOdd(numbers);
 		
 	}
