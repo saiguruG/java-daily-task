bfcf5e7d3913a7cee431c740393a9d823cc1ac07
saiguruG <17saiguru@gmail.com>
2024-11-19 04:11:15 -0800

    Update EvenorAdd.java

--- EvenorAdd.java
+++ EvenorAdd.java
@@ -19,7 +19,7 @@ public class EvenorAdd {
 		
 	}
 	public static void main (String[]args ) {
-		int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+		int numbers = {1,2,3,4,5,6,7,8,9,10};
 		printEvenorOdd(numbers);
 		
 	}
