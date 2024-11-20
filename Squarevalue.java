4f75a82adda58a259d9943c3bf3c9934a1b704fc
saiguruG <17saiguru@gmail.com>
2024-11-20 00:22:28 -0800

    Add files via upload

---
+++ Squarevalue.java
@@ -0,0 +1,14 @@
+package javaproject;
+import java.util.*;
+public class Squarevalue {
+	public static void main(String[]args) {
+		Scanner obj = new Scanner(System.in);
+		System.out.println("enter the values : ");
+		int num=obj.nextInt();
+		int square=num*num;
+		System.out.println("print the value of perfect square: " +square);
+	}
+	
+	
+
+}
